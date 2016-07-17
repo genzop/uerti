f89bd151998d2711c35e7b00a53266ddede23785
Enzo Panettieri <genzop@gmail.com>
2016-07-17 16:04:56 -0300

    Added RTCWebDemo content

--- ios/RCTWebRTCDemo/AppDelegate.m
+++ ios/RCTWebRTCDemo/AppDelegate.m
@@ -22,7 +22,7 @@
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"uerti"
+                                                      moduleName:@"RCTWebRTCDemo"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
